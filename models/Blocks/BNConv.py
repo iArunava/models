7d5d04859cd97f81cd9c622be151fa1e07d8b086
iarunava <learningdroidarunava@gmail.com>
2019-02-23 09:54:43 +0530

    Updated BNConv Block

--- models/Blocks/BNConv.py
+++ models/Blocks/BNConv.py
@@ -4,21 +4,32 @@ import torch.nn as nn
 class BNConv(nn.Module):
 
     def __init__(self, in_channels, out_channels, kernel_size, stride,
-                 padding, eps, momentum):
+                 padding, bias=False, eps=1e-5, momentum=0.1, conv_first=True):
         '''
         '''
 
         super(BNConv, self).__init__()
         
-        self.main = nn.Sequential(
-                    nn.Conv2d(in_channels=in_channels,
-                                          out_channels=out_channels,
-                                          stride=stride,
-                                          padding=padding,
-                                          bias=bias),
-                    
-                    nn.BatchNorm2d(out_channels, eps=eps, momentum=momentum)
-                )
+        if conv_first:
+            self.main = nn.Sequential(
+                        nn.Conv2d(in_channels=in_channels,
+                                              out_channels=out_channels,
+                                              stride=stride,
+                                              padding=padding,
+                                              bias=bias),
+                        
+                        nn.BatchNorm2d(out_channels, eps=eps, momentum=momentum)
+                    )
+        else:
+            self.main = nn.Sequential(
+                            nn.BatchNorm2d(in_channels, eps=eps, momentum=momentum),
+
+                            nn.Conv2d(in_channels=in_channels,
+                                      out_channels=out_channels,
+                                      stride=stride,
+                                      padding=padding,
+                                      bias=bias)
+                        )
 
 
     def forward(self, x):
